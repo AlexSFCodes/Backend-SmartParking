a12b30a919ee11bfb9dd3d5acf0bafde1eb7bb6e
AlexSFCodes <darixsango@hotmail.com>
2026-06-09 21:17:07 -0500

    base de datos conectada

--- src/domain/repositories/IUsuarioRepository.ts
+++ src/domain/repositories/IUsuarioRepository.ts
@@ -3,5 +3,5 @@ import { Usuario } from '../entities/usuario';
 export abstract class Usuarios {
     abstract buscarPorEmail(email: string): Promise<Usuario | null>;
     abstract guardar(usuario: Usuario): Promise<void>;
-    abstract buscarPorId(id: string): Promise<Usuario | null>;
+    abstract buscarPorId(id: number): Promise<Usuario | null>;
 }
---
+++ src/infrastructure/repositories/MysqlUsuarioRepository.ts
@@ -0,0 +1,46 @@
+import { Usuarios } from '../../domain/repositories/IUsuarioRepository';
+import { pool } from './database/mysql.connection';
+import { Usuario } from '../../domain/entities/usuario';
+export class UsuarioMySQL extends Usuarios{
+    async buscarPorEmail(email: string): Promise<Usuario | null> {
+        const [rows]: any = await pool.execute(
+            'SELECT * FROM usuarios WHERE email = ?',
+            [email]
+        );
+
+        if (rows.length === 0) {
+            return null;
+        }
+
+        const fila = rows[0];
+        const usuario = new Usuario(fila.id_rol, fila.email, fila.nombre);
+        usuario.passwordHash = fila.password_hash;
+        usuario.activo = fila.activo;
+
+        return usuario;
+    }
+    async buscarPorId(id: number): Promise<Usuario | null> {
+        const [rows]: any = await pool.execute(
+            'SELECT * FROM usuarios WHERE id_usuario = ?',
+            [id]
+        );
+
+        if (rows.length === 0) {
+            return null;
+        }
+
+        const fila = rows[0];
+        const usuario = new Usuario(fila.id_rol, fila.email, fila.nombre);
+        usuario.passwordHash = fila.password_hash;
+        usuario.activo = fila.activo;
+
+        return usuario;
+    }
+
+    async guardar(usuario: Usuario): Promise<void> {
+        await pool.execute(
+            'INSERT INTO usuarios (nombre, email, password_hash, id_rol, activo) VALUES (?, ?, ?, ?, ?)',
+            [usuario.nombre, usuario.email, usuario.passwordHash, usuario.idRol, usuario.activo]
+        );
+    }
+}
